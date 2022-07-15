358c72dccea2b4aaa09c6e83b5138bd6e6a03f62
shiva-kant <shivakant.choudhary@gmail.com>
2022-07-15 10:36:01 +0530

    first commit

---
+++ TestNgDemo/src/main/java/my/MyMaths.java
@@ -0,0 +1,10 @@
+package my;
+
+public class MyMaths {
+
+	public int add(int x,int y)
+	{
+		return x+y;
+	}
+	
+}
---
+++ TestNgDemo/src/test/java/my/MyMathsTest.java
@@ -0,0 +1,36 @@
+package my;
+
+import static org.junit.Assert.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class MyMathsTest {
+	WebDriver driver;
+
+	@BeforeClass
+	public void init() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\shivchou\\Downloads\\chromedriver.exe");
+		driver = new ChromeDriver();
+driver.get("C:\\Users\\shivchou\\Desktop\\hello.html");
+	}
+
+	@AfterClass
+	public void destroy() {
+
+		driver = null;
+		// System.out.println("AFTER");
+	}
+
+	@Test
+	public void testOne() {
+		WebElement element = driver.findElement(By.xpath("//*[@id=\"xyz\"]"));
+		String str = element.getText();
+		assertEquals("DIV", str);
+	}
+}
